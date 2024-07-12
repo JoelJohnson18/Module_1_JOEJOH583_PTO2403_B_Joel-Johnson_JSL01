3003a8fb88e824c70bde2cd944ee8acf8642f96e
JoelJohnson18 <joeljohnson180798@gmail.com>
2024-07-12 19:52:12 +0200

    Done

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function validateSyntax() {
   let regex = /^pet_[a-zA-Z0-9]+$/;
 
   if (regex.test(input)) {
-    result = "Valid input";
+    result = '<span class="valid">Valid syntax &#x1F7E2;</span>'; // Green circle
   } else {
-    result = "Invalid input";
+    result = '<span class="invalid">Invalid syntax &#x1F534;</span>'; // Red circle
   }
 
-  document.getElementById("result").innerText = result;
+  document.getElementById("result").innerHTML = result;
 }
